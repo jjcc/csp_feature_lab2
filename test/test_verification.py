d42fbce7871884dff14a36bb3347ffcf712931dc
jjcc <jjccforth@gmail.com>
2025-11-01 17:26:07 -0400

    fill the close price, filter weekly if the flag is true

--- test/test_verification.py
+++ test/test_verification.py
@@ -3,12 +3,34 @@ import os
 import re
 import shutil
 import unittest
-from dotenv import load_dotenv
-import joblib
+import numpy as np
 import pandas as pd
-from service.utils import prep_tail_training_df, fill_features_with_training_medians
-from service.utils import ALL_FEATS
-from train_tail_with_gex import _fill_features
+from service.data_prepare import lookup_close_on_or_before, preload_prices_with_cache
+
+
+
+# Use preloaded prices to compute expiry_close
+def expiry_close_from_cache(r, preload_closes):
+    if preload_closes is None:
+        return np.nan
+    
+    if 'expirationDate' not in r:
+        symbol = str(r['symbol']).upper()
+        _, expiry_date, _ = symbol.split('|')
+        expiry_date = expiry_date[:4] + '-' + expiry_date[4:6] + '-' + expiry_date[6:]
+        r['expirationDate'] = expiry_date
+
+
+    # Don't label future expiration dates
+    expiry_date = pd.to_datetime(r['expirationDate'])
+    if pd.isna(expiry_date) or expiry_date > pd.Timestamp.now():
+        return np.nan
+
+    sym = str(r['baseSymbol']).upper()
+    price_df = preload_closes.get(sym)
+    # Only allow prices within 3 trading days of expiration
+    return lookup_close_on_or_before(price_df, expiry_date, max_days_back=3) if price_df is not None else np.nan
+
 
 class TestVerification(unittest.TestCase):
 
@@ -87,6 +109,68 @@ class TestVerification(unittest.TestCase):
         # get the weekday of that date
         weekday = pd.to_datetime(date_segment).day_name()
         return date_segment, weekday
+
+
+    def test_fill_expiry_close(self):
+        """
+        Fill expiry_close column for all files in monday_only folder.
+        The result file will be saved in labeled folder.
+        """
+        from dotenv import load_dotenv
+        load_dotenv(".env.test")
+        monday_only_dir = os.getenv("MONDAY_ONLY_DIR", "prod/output/monday_only")
+        files = glob.glob(os.path.join(monday_only_dir, "*.csv"))
+        print(f"Found {len(files)} files to process expiry_close")
+        for file in files:
+            print(f"Processing expiry_close for {file}")
+            self.fill_expiry_close(file)
+
+        
+
+
+    def fill_expiry_close(self, file, filter_weekly=True):
+        """
+        Function to fill expiry_close column for a given file, and save to labeled folder.
+        """
+        labled_output_dir = os.getenv("LABELED_OUTPUT_DIR", "prod/output/labeled")
+        df = pd.read_csv(file)
+
+        # compensate the expiry_close column
+        if 'expirationDate' not in df.columns:
+            df['expirationDate'] = df['symbol'].apply(
+                lambda s: str(s).upper()[str(s).upper().find('|')+1:str(s).upper().find('|')+9]).apply(
+                    lambda d: d[:4] + '-' + d[4:6] + '-' + d[6:])
+        
+
+
+        syms = df['baseSymbol'].dropna().astype(str).str.upper().unique().tolist()
+
+        if 'tradeTime' not in df.columns:
+            file_name = file.split('/')[-1]
+            date_segment, _ = self.get_date_seg(file_name)
+            #tt = pd.to_datetime(date_segment)
+            df['tradeTime'] = date_segment
+        tt = pd.to_datetime(df.get('tradeTime', pd.NaT), errors="coerce")
+        ed = pd.to_datetime(df.get('expirationDate', pd.NaT), errors="coerce")
+
+
+        # filter for weekly options
+        if filter_weekly:
+            # only keep weekly options (daysToExpiration <=7)
+            if 'daysToExpiration' not in df.columns:
+                df['daysToExpiration'] = (ed - tt).dt.days
+            df = df[df['daysToExpiration'] <= 7].copy()
+
+
+        cache_dir = os.getenv("COMMON_OUTPUT_DIR", "./output")
+        batch_size = int(os.getenv("DATA_BATCH_SIZE", "30"))
+        cut_off_date =  pd.Timestamp.now()
+
+        preload_closes = preload_prices_with_cache(syms, tt, ed, cache_dir, batch_size=batch_size, cut_off_date=cut_off_date)
+
+        df['expiry_close'] = df.apply(lambda r: expiry_close_from_cache(r, preload_closes), axis=1)
+        output_file = f"{labled_output_dir}/{file.split('/')[-1]}"
+        df.to_csv(output_file, index=False)
 
 
 if __name__ == '__main__':
